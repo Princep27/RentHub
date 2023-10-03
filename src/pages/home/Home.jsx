62a5dfe21337baa37ba971d70628467f5e24c80c
CaptainAwesomeRaj <100113570+CaptainAwesomeRaj@users.noreply.github.com>
2023-10-03 13:17:32 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./home.css";
 import Header from '../../components/header/Header';
 import Products from '../../components/products/Products';
-
+import Footer from '../../components/footer/Footer';
 export default function Home() {
   return (
     <>
@@ -13,6 +13,7 @@ export default function Home() {
               <Products/>
           </div>
         </div>
+        <Footer/>
     </>
   )
 }
